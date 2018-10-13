b91b3f26ff8b65e40e54d35631794a33ddf7ee78
KatherineJY <kellymayliu@163.com>
2018-10-13 14:44:45 +0800

    Add crimes list

--- app/src/main/java/com/ghy/katherinejy/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/ghy/katherinejy/criminalintent/SingleFragmentActivity.java
@@ -1,26 +1,23 @@
 package com.ghy.katherinejy.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
 
-public class CrimeActivity extends SingleFragmentActivity {
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    protected abstract Fragment createFragment();
+
     @Override
-    protected Fragment createFragment() {
-        return new CrimeFragment();
-    }
-
-    /*@Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
-
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
         if(fragment==null){
-            fragment = new CrimeFragment();
+            fragment =createFragment();
             fm.beginTransaction().add(R.id.fragmentContainer,fragment).commit();
         }
-    }*/
+    }
 }
