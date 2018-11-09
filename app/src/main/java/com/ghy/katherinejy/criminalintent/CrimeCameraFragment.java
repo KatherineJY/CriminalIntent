2db5029f31d110c79af2c5ede57c4ba33e22eb29
KatherineJY <kellymayliu@163.com>
2018-11-09 18:04:48 +0800

    create camera fragment

---
+++ app/src/main/java/com/ghy/katherinejy/criminalintent/CrimeCameraFragment.java
@@ -0,0 +1,30 @@
+package com.ghy.katherinejy.criminalintent;
+
+import android.graphics.Camera;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class CrimeCameraFragment extends Fragment {
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceSate){
+        View v = inflater.inflate(R.layout.fragment_crime_camera,parent,false);
+        Button takePictureButton = (Button)v.findViewById(R.id.crime_camera_takePictureButton);
+        takePictureButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+        mSurfaceView = (SurfaceView) v.findViewById(R.id.crime_camera_surfaceView);
+        return v;
+    }
+}
