7be79553e23b5c4f1ab7b56ce1ee342ef158b348
KatherineJY <kellymayliu@163.com>
2018-11-12 17:51:13 +0800

    propertites

--- app/src/main/java/com/ghy/katherinejy/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/com/ghy/katherinejy/criminalintent/CrimeCameraFragment.java
@@ -1,9 +1,12 @@
 package com.ghy.katherinejy.criminalintent;
 
-import android.graphics.Camera;
+import android.annotation.TargetApi;
+import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +28,64 @@ public class CrimeCameraFragment extends Fragment {
             }
         });
         mSurfaceView = (SurfaceView) v.findViewById(R.id.crime_camera_surfaceView);
+        SurfaceHolder holder = mSurfaceView.getHolder();
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try{
+                    if( mCamera != null ){
+                        mCamera.setPreviewDisplay(holder);
+                    }
+                }
+                catch (Exception e){
+
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                if( mCamera==null ) {
+                    Camera.Parameters parameters = mCamera.getParameters();
+                    Camera.Size s = null;
+                    parameters.setPreviewSize(s.width,s.height);
+                    mCamera.setParameters(parameters);
+                    try {
+                        mCamera.startPreview();
+                    }
+                    catch( Exception e ) {
+                        mCamera.release();
+                        mCamera = null;
+                    }
+                }
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                if( mCamera!=null ) {
+                    mCamera.stopPreview();
+                }
+            }
+        });
         return v;
+    }
+
+    @TargetApi(9)
+    @Override
+    public void onResume(){
+        super.onResume();
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.GINGERBREAD) {
+            mCamera = Camera.open(0);
+        } else {
+            mCamera = Camera.open();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mCamera!=null){
+            mCamera.release();
+            mCamera=null;
+        }
     }
 }
